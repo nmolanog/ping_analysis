b98565b0153a63b6c0be21689d980214a45b5e02
nmolanog <nmolanog@gmail.com>
2020-06-12 14:23:59 -0500

    improving layout

--- R/Load_from_raw.R
+++ R/Load_from_raw.R
@@ -16,7 +16,7 @@ output_path<-"../outputs"
 ####see available xlsx files to load
 list.files("../data/raw")%>%str_subset(".txt")
 ###asign the apropiate name file. without xlsx extencion
-test_name<-"test_2020_06_12__1105"
+test_name<-"test_2020_06_12__1312"
 file_nm<-paste0(test_name,".txt")
 ###load file
 
@@ -53,34 +53,39 @@ tail(z2)
 treshld_píng<-round(mean(z2$ping,na.rm = T)+2*sd(z2$ping,na.rm = T),2)
 my_breaks <- c(seq(0, max(z2$ping,na.rm = T), length.out = 5),treshld_píng)
 my_labels <- as.character(my_breaks)
+z2$min<-(z2$time_sc_rel/60)%>%cut(breaks=c(-Inf,1:15),labels = 0:14)
+
+ping_anom_count<-z2[z2$ping>treshld_píng,"min"]%>%table%>%{data.frame(.)}
+colnames(ping_anom_count)<-c("minuto","anomalos")
 
 p1<-z2%>%ggplot(aes(x=time_sc_rel,y=ping))+geom_line()+
-  scale_x_continuous(name ="minutos",breaks = (0:15)*60,labels =0:15)+theme_bw()+
-  geom_hline(yintercept=treshld_píng,linetype="dashed", color = "red")+
-  scale_y_continuous(limits = c(0, max(z2$ping)), breaks = my_breaks, labels = my_labels,
-                     name = "ping (ms)")
-p2<-z2%>%ggplot(aes(x=ping))+geom_density()+theme_bw()
+scale_x_continuous(name ="minutos",breaks = (0:15)*60,labels =0:15)+theme_bw()+
+geom_hline(yintercept=treshld_píng,linetype="dashed", color = "red")+
+scale_y_continuous(limits = c(0, max(z2$ping)), breaks = my_breaks, labels = my_labels,
+                   name = "ping (ms)")+ ggtitle('ping en tiempo real')
+p2<-z2%>%ggplot(aes(x=ping))+geom_density()+theme_bw()+ ggtitle('distribucion del ping')
 
-p3<-ggplot() + 
-  annotate("text", x = 3.5, y = 35, size=3, label = z_info[1])+
-  annotate("text", x = 3, y = 15, size=3, label = "ping stats")+
-  annotate("text", x = 4, y = 30, size=3, label = paste0("min: ", round(min(z2$ping,na.rm = T),3)))+
-  annotate("text", x = 4, y = 25, size=3, label = paste0("Q1: ", round(quantile(z2$ping,0.25,na.rm = T),3)))+
-  annotate("text", x = 4, y = 20, size=3, label = paste0("median: ", round(quantile(z2$ping,0.5,na.rm = T),3)))+
-  annotate("text", x = 4, y = 15, size=3, label = paste0("mean: ", round(mean(z2$ping,na.rm = T),3)))+
-  annotate("text", x = 4, y = 10, size=3, label = paste0("Q3: ", round(quantile(z2$ping,0.75,na.rm = T),3)))+
-  annotate("text", x = 4, y = 5, size=3, label = paste0("max: ", round(max(z2$ping,na.rm = T),3)))+
-  annotate("text", x = 4, y = 0, size=3, label = paste0("sd: ", round(sd(z2$ping,na.rm = T),3)))+
-  annotate("text", x = 3.5, y = -5, size=3, label = z_info[2])+
-  scale_x_continuous(limits = c(1, 6)) + scale_y_continuous(limits = c(-6, 37)) +
-  theme_void() + theme(legend.position="none")
+p3<-tableGrob(data.frame(stat=c("min","Q1","median","mean","Q3","max","sd"),
+                        value=c(round(min(z2$ping,na.rm = T),3),
+                                round(quantile(z2$ping,0.25,na.rm = T),3),
+                                round(quantile(z2$ping,0.5,na.rm = T),3),
+                                round(mean(z2$ping,na.rm = T),3),
+                                round(quantile(z2$ping,0.75,na.rm = T),3),
+                                round(max(z2$ping,na.rm = T),3),
+                                round(sd(z2$ping,na.rm = T),3)
+                                )),rows =NULL) 
+
+p4<-ping_anom_count%>%ggplot(aes(x=minuto, y=anomalos)) +
+  geom_bar(stat="identity")+theme_bw()+ ggtitle('numero de paquetes con ping mayor al humbral')
 
 pdf(file=paste0(output_path,"/",test_name,".pdf"),width=12,height=6)
 grid.arrange(
-  grobs = list(p1,p2,p3),
+  grobs = list(p1,p2,p4,p3),
   widths = c(1,1,1),
   layout_matrix = rbind(c(1,1,1),
-                        c(2,3,3))
+                        c(2,3,4)),
+  top=z_info[1],
+  bottom=z_info[2]
 )
 dev.off()
 
