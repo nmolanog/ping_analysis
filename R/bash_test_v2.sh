328ff0b58dc91a6e4a4d2950d6acf6f229e57792
nmolanog <nmolanog@gmail.com>
2020-06-15 15:40:06 -0500

    default arguments created for the script. remember to create folders data and outputs in next step

--- R/bash_test_v2.sh
+++ R/bash_test_v2.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+
+timeout_time=${1:-"10s"}
+intbtweentrans=${2:-1}
+urltoping=${3:-"www.google.com"}
 name_file="test_$(date +"%Y_%m_%d__%H%M")"
+
+echo "Total Arguments:" $#
+echo "All Arguments values:" $@
+
+echo "First->"  $1
+echo "Second->" $2
+echo "Third->"  $3
+
 echo $name_file
-sudo timeout 120s ping -i 1 www.google.com  | 
+
+
+sudo timeout $timeout_time ping -i $intbtweentrans $urltoping  | 
 while read pong; do   
   echo "$(date +"%T.%N"): $pong"
 done > "../data/raw/$name_file.txt"
 
 ./bash_script_v2.r "$name_file.txt"
+
+xdg-open "../outputs/$name_file.pdf"
 
